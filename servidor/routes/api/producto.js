8130b75bfedc7880f082bf56887474982f46feae
mataix.ab5 <mataix.ab@gmail.com>
2021-09-24 17:00:15 +0200

    Operaciones CRUD terminadas con producto

--- servidor/routes/api/producto.js
+++ servidor/routes/api/producto.js
@@ -59,6 +59,60 @@ router.post("/", async (req, res) => {
   }
 });
 
+// DELTE -> Borramos producto, lo buscamos por id y si existe, lo borramos
 
+router.delete("/:id", async (req, res) => {
+
+    try {
+      let producto = await Producto.findById(req.params.id);
+
+      if(!producto) {
+          res.status(404).json({ msg: 'No existe el producto'})
+      }else{
+
+        await Producto.findOneAndRemove({ _id:req.params.id})
+
+        res.json({ msg: 'Producto eliminado con éxito!' })
+      }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error al borrar el producto');
+    }
+
+ });
+
+
+ // PUT -> Actualizamos producto, lo buscamos por id, y si existe, se actualiza 
+
+ router.put("/:id", async (req, res) => {
+
+  try {
+    const {nombre,tipo,marca,modelo,estado,precio,descripcion,imagen,ubicacion} = req.body;
+    let producto = await Producto.findById(req.params.id);
+
+    if(!producto) {
+        res.status(404).json({ msg: 'No existe el producto'})
+    }
+
+    producto.nombre = nombre;
+    producto.tipo = tipo;
+    producto.marca = marca;
+    producto.modelo = modelo;
+    producto.estado = estado;
+    producto.precio = precio;
+    producto.descripcion = descripcion;
+    producto.imagen = imagen;
+    producto.ubicacion = ubicacion;
+
+    producto = await Producto.findOneAndUpdate({ _id:req.params.id},producto, { new:true })
+    res.json(producto)
+    
+} catch (error) {
+    console.log(error);
+    res.status(500).send('Hubo un error');
+}
+
+});
 
   module.exports = router;
