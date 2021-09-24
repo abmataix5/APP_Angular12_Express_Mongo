a2a1beda0bf3240f779849102e160c7adb7e7729
mataix.ab5 <mataix.ab@gmail.com>
2021-09-24 17:54:43 +0200

    Estructura cliente y nav bar

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    DetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
