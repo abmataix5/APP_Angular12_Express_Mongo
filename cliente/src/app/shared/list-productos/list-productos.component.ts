408d43db11f9ac2b99240bdec6d4a54757d64b14
mataix.ab5 <mataix.ab@gmail.com>
2021-09-28 12:17:07 +0200

    Shop List funcionando

--- cliente/src/app/shared/list-productos/list-productos.component.ts
+++ cliente/src/app/shared/list-productos/list-productos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { ProductoService } from '../../core';
+import { ProductoService,Producto } from '../../core';
 
 @Component({
   selector: 'app-list-productos',
@@ -9,6 +9,8 @@ import { ProductoService } from '../../core';
 })
 export class ListProductosComponent implements OnInit {
 
+  listProductos: Producto[] = [];
+  
   constructor( private aRouter: ActivatedRoute,private router: Router, private _productoService: ProductoService,) { }
 
 
@@ -21,7 +23,7 @@ export class ListProductosComponent implements OnInit {
       this._productoService.getProductos().subscribe(
         (data) => {
           console.log(data);
-
+            this.listProductos = data;
         },
         (error) => {
         
