a85a20fbceb969b6ea79d81b765ed1fcf0ec275f
mataix.ab5 <mataix.ab@gmail.com>
2021-09-30 10:55:13 +0200

    Avance en categorias cliente

--- cliente/src/app/shared/index.ts
+++ cliente/src/app/shared/index.ts
@@ -2,3 +2,4 @@ export * from './layout';
 export * from './shared.module';
 export * from './list-productos';
 export * from './list-productos/list-productos.component';
+export * from './categories';
